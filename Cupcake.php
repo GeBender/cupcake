9f0854f12294fd0a10c6ce4004c05d8dcd435c34
GeBender <gesianbender@gmail.com>
2014-11-12 14:32:47 -0300

    Primeiro commit. Classe oca

---
+++ Cupcake.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * (c) Cupcake: Rapid and Tasty Development Framework.
+ *
+ * PHP version 5.5.12
+ *
+ * @author    Ge Bender <gesianbender@gmail.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @version   GIT: <git_id>
+ * @link      http://cupcake.simplesys.com.br
+ */
+ 
+namespace Cupcake;
+
+/**
+ * Cupcake is a autoRouter and autoRender microFramework based on the Symfony2 Components.
+ * Controllers, actions and views automatic executed by the url path.
+ */
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+class Cupcake
+{
+
+	/**
+	 * Run the Framework and do all the magic.
+	 */
+	public function run()
+	{
+		return 'faz de conta...';
+	}
+}
+?>
